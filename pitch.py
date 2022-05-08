4809714b47aab76b02584f55a1a26a6a88498f8e
Olliemint <furywrld5@gmail.com>
2022-05-08 18:38:55 +0300

    created classes for User and Pitches

--- pitch.py
+++ pitch.py
@@ -1,5 +1,6 @@
 
-from enum import unique
+from datetime import date
+
 from flask import Flask, render_template,url_for,flash,redirect
 from form import RegistrationForm,LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -11,9 +12,33 @@ app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id =db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
     username =db.Column(db.String(15),unique=True, nullable=False)
     email =db.Column(db.String(100),unique=True, nullable=False)
+    image_file =db.Column(db.String(60), nullable=False, default ='default.jpg')
+    password =db.Column(db.String(60), nullable=False)
+    pitchs = db.relationship('Pitch',backref = 'author', lazy =True)
+    def __repr__(self):
+        
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String(100), nullable=False)
+    posted = db.Column(db.DateTime,nullable = False, default = date.ctime)
+    pitch = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable = False)
+    
+    
+    
+    
+    def __repr__(self):
+        
+        return f"Pitch('{self.title}','{self.posted}')"
+    
+    
+    
+
 
 
 
